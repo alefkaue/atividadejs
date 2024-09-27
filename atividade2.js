9ae1d59cca113dd4a5af910509f6fc2257cbbd5b
Alef Santos <167534821+alefkaue@users.noreply.github.com>
2024-09-27 16:54:52 -0300

    Add files via upload

---
+++ atividade2.js
@@ -0,0 +1,42 @@
+const numeroQueQueroRegistrar = 31472
+const stringTemplate = `O número que decidi regitrar foi o ${numeroQueQueroRegistrar}`;
+console.log(stringTemplate)
+
+//------------------
+
+const a = 10;
+const b = 20;
+const stringTemplate1 = `O resultado da soma é ${a+b}`;
+console.log(stringTemplate1)
+
+//------------------
+
+const nome = 'Alef';
+const idade = 17;
+const stringTemplate2 = `Oi, meu nome é ${nome}!
+Minha idade é ${idade}`;
+console.log(stringTemplate2)
+
+//------------------
+
+const numero1 = 27;
+const numero2 = 23.25;
+const numero3 = -40;
+const total = numero1 + numero2 + numero3;
+
+console.log(total);
+
+//------------------
+
+const valor1 = true;
+const valor2 = false;
+
+const valor1Elavor2 = valor1 && valor2;
+const valor1OUValor2 = valor1 || valor2;
+const inversoValor1 = !valor1;
+const inversoValor2 = !valor2;
+
+console.log(valor1Elavor2);
+console.log(valor1OUValor2);
+console.log(inversoValor1);
+console.log(inversoValor2);
